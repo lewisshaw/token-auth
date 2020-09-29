801fcd603d807b6eff5755ba26552fa4efeecdc2
Lewis Shaw <lshaw45@gmail.com>
2020-09-29 21:42:03 +0100

    Remove expiry date

--- src/TokenAuth/Data/Entity/Token.php
+++ src/TokenAuth/Data/Entity/Token.php
@@ -2,26 +2,17 @@
 
 namespace TokenAuth\Data\Entity;
 
-use DateTimeInterface;
-
 class Token
 {
     private string $token;
-    private DateTimeInterface $expiryDate;
 
-    public function __construct(string $token, DateTimeInterface $expiryDate)
+    public function __construct(string $token)
     {
         $this->token = $token;
-        $this->expiryDate = $expiryDate;
     }
 
     public function getToken(): string
     {
         return $this->token;
-    }
-
-    public function getExpiryDate(): DateTimeInterface
-    {
-        return $this->expiryDate;
     }
 }
--- src/TokenAuth/Process/Response/GenerateTokenResponse.php
+++ src/TokenAuth/Process/Response/GenerateTokenResponse.php
@@ -7,21 +7,14 @@ use DateTimeInterface;
 class GenerateTokenResponse
 {
     private string $token;
-    private DateTimeInterface $expiryDate;
 
-    public function __construct(string $token, DateTimeInterface $expiryDate)
+    public function __construct(string $token)
     {
         $this->token = $token;
-        $this->expiryDate = $expiryDate;
     }
 
     public function getToken(): string
     {
         return $this->token;
-    }
-
-    public function getExpiryDate(): DateTimeInterface
-    {
-        return $this->expiryDate;
     }
 }
