d5f892dcd489c14f96c92e842e19b09c84b5874c
Lewis Shaw <lshaw45@gmail.com>
2020-10-01 21:03:46 +0100

    Fix password updater

--- src/TokenAuth/Process/User/UpdateUserPassword.php
+++ src/TokenAuth/Process/User/UpdateUserPassword.php
@@ -4,6 +4,7 @@ namespace TokenAuth\Process\User;
 
 use TokenAuth\Data\Repository\User\UserInterface;
 use TokenAuth\Process\Password\PasswordHasherInterface;
+use TokenAuth\Process\Password\PasswordVerifierInterface;
 use TokenAuth\Process\Request\UpdateUserPasswordRequest;
 use TokenAuth\Process\Response\UpdateUserPasswordResponse;
 
@@ -11,20 +12,26 @@ class UpdateUserPassword
 {
     private UserInterface $userRepo;
     private PasswordHasherInterface $passwordHasher;
+    private PasswordVerifierInterface $passwordVerifier;
 
     public function __construct(
         UserInterface $userRepo,
-        PasswordHasherInterface $passwordHasher
+        PasswordHasherInterface $passwordHasher,
+        PasswordVerifierInterface $passwordVerifier
     ) {
         $this->userRepo = $userRepo;
         $this->passwordHasher = $passwordHasher;
+        $this->passwordVerifier = $passwordVerifier;
     }
 
     public function updatePassword(UpdateUserPasswordRequest $request): UpdateUserPasswordResponse
     {
         $user = $this->userRepo->getUserById($request->getUserId());
-        $oldPasswordHashed = $this->passwordHasher->hash($request->getOldRawPassword());
-        if ($oldPasswordHashed !== $user->getPassword()) {
+        $oldPasswordCorrect = $this->passwordVerifier->isPasswordCorrect(
+            $request->getOldRawPassword(),
+            $user->getPassword()
+        );
+        if (!$oldPasswordCorrect) {
             return new UpdateUserPasswordResponse(false, false);
         }
         $newPasswordHashed = $this->passwordHasher->hash($request->getNewRawPassword());
--- test/TokenAuth/Process/User/UpdateUserPasswordTest.php
+++ test/TokenAuth/Process/User/UpdateUserPasswordTest.php
@@ -7,19 +7,23 @@ use TokenAuth\Process\Password\PasswordHasherInterface;
 use TokenAuth\Process\Request\UpdateUserPasswordRequest;
 use TokenAuth\Process\User\UpdateUserPassword;
 use PHPUnit\Framework\TestCase;
+use TokenAuth\Process\Password\PasswordVerifierInterface;
 
 class UpdateUserPasswordTest extends TestCase
 {
     private $userRepo;
     private $passwordHasher;
+    private $passwordVerifier;
 
     protected function setUp(): void
     {
         $this->userRepo = $this->createMock(UserInterface::class);
         $this->passwordHasher = $this->createMock(PasswordHasherInterface::class);
+        $this->passwordVerifier = $this->createMock(PasswordVerifierInterface::class);
         $this->process = new UpdateUserPassword(
             $this->userRepo,
-            $this->passwordHasher
+            $this->passwordHasher,
+            $this->passwordVerifier
         );
     }
 
@@ -31,7 +35,7 @@ class UpdateUserPasswordTest extends TestCase
             'Test Name'
         );
         $this->userRepo->method('getUserById')->willReturn($user);
-        $this->passwordHasher->method('hash')->willReturn('321');
+        $this->passwordVerifier->method('isPasswordCorrect')->willReturn(false);
         $updatePasswordRequest = new UpdateUserPasswordRequest(
             1,
             '333',
@@ -50,8 +54,9 @@ class UpdateUserPasswordTest extends TestCase
             'Test Name'
         );
         $this->userRepo->method('getUserById')->willReturn($user);
+        $this->passwordVerifier->method('isPasswordCorrect')->willReturn(true);
         $this->passwordHasher->method('hash')
-            ->willReturnOnConsecutiveCalls('123', '545');
+            ->willReturn('545');
         $updatePasswordRequest = new UpdateUserPasswordRequest(
             1,
             '321',
